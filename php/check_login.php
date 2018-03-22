1b024f6c1501a3d76ebe76de28e5af3f45857a75
Nicolas Ward <nic@as1515-mbpro.nic>
2018-03-22 13:46:04 -0400

    Session expiration upped

--- php/check_login.php
+++ php/check_login.php
@@ -1,4 +1,7 @@
 <?php
+	// 10 year session expiration
+	ini_set('session.gc_maxlifetime', 315360000);
+
 	// Check to make sure the user is logged in
 	session_start();
 	if (!isset($_SESSION['email'])) {

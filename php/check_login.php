e9496eb4b4c7c782699a473bca7e5dc2647c7b7d
Nicolas Ward <nic@as1515-mbpro.nic>
2018-04-19 14:34:09 -0400

    Removing 10 year session expiration

--- php/check_login.php
+++ php/check_login.php
@@ -1,7 +1,4 @@
 <?php
-	// 10 year session expiration
-	ini_set('session.gc_maxlifetime', 315360000);
-
 	// Check to make sure the user is logged in
 	session_start();
 	if (!isset($_SESSION['email'])) {
